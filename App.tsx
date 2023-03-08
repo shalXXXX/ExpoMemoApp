191909e81a5ab7b8927ba1eea265608fed7bb5a3
shalXXXX <hibiki.a.h.0618@gmail.com>
2023-03-08 17:33:32 +0900

    Style MemoList Screen

--- App.tsx
+++ App.tsx
@@ -5,18 +5,46 @@ import Hello from './src/components/Hello';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang={true}>World</Hello>
-      <Hello
-        style={{
-          fontSize: 16
-        }}
-        >
-          World
-      </Hello>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.appBar}>
+        <View style={styles.appBarInner}>
+          <Text style={styles.appBarTitle}>Memo App</Text>
+          <Text style={styles.appBarRight}>ログアウト</Text>
+        </View>
+      </View>
 
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2020年12月24日</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.circleBotton}>
+        <Text style={styles.circleBottonLabel}>+</Text>
+      </View>
     </View>
   );
 }
@@ -24,8 +52,71 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F0F4F8',
   },
+  appBar: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    width: "100%",
+    justifyContent: "flex-end",
+  },
+  appBarInner: {
+    alignItems: "center",
+  },
+  appBarTitle: {
+    fontSize: 32,
+    color: "white",
+    fontWeight: "500",
+    lineHeight: 32,
+    marginBottom: 8,
+  },
+  appBarRight: {
+    position: "absolute",
+    right: 19,
+    bottom: 16,
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+
+  memoListItem: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484"
+  },
+  circleBotton: {
+    backgroundColor: "#467FD3",
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    // iOSのシャドウ
+    shadowColor: "#000000",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.75,
+    shadowRadius: 8,
+    // androidのシャドウ
+    elevation: 8,
+  },
+  circleBottonLabel: {
+    color: "#fff",
+    fontSize: 40,
+    lineHeight: 46,
+  }
 });
