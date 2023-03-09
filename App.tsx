451f38ad91688bbfbdd16b06b864f92d17ef5d45
shalXXXX <hibiki.a.h.0618@gmail.com>
2023-03-09 18:29:07 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 
 import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <MemoListScreen />
+    // <MemoListScreen />
     // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   )
 }
